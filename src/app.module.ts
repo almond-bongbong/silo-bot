5673cd14ac9d752c67401aa57d9690c536641b3e
cmlee <bal.dongdongdong@gmail.com>
2021-08-20 20:58:06 +0900

    (feature) ready for slack notification

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationModule } from './notification/notification.module';
+import { NotificationService } from './notification/notification.service';
+import { ConfigModule } from '@nestjs/config';
+import { isDevelopment } from './lib/environment';
+import { CRAWLER_NOTIFICATION_CHANNEL } from './notification/notification.constant';
+
+console.log('Environment :', process.env.NODE_ENV);
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // ignoreEnvFile: isProduction,
+      envFilePath: isDevelopment ? '.env.development' : '.env.production',
+    }),
     NotificationModule.forRoot({
       slackBotOauthToken: process.env.SLACK_BOT_OAUTH_TOKEN,
     }),
@@ -12,4 +23,11 @@ import { NotificationModule } from './notification/notification.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly notificationService: NotificationService) {
+    notificationService.postMessage(
+      CRAWLER_NOTIFICATION_CHANNEL,
+      'Server started! 🚀',
+    );
+  }
+}
--- src/notification/notification.constant.ts
+++ src/notification/notification.constant.ts
@@ -1,1 +1,2 @@
 export const CONFIG_OPTIONS = 'CONFIG_OPTIONS';
+export const CRAWLER_NOTIFICATION_CHANNEL = '#crawler-notification';
